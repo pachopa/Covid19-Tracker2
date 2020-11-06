c519879037ff3c8bdd89b6e44798111e960682a7
Chris Lee <chrislee910320@gmail.com>
2020-11-06 14:13:24 -0700

    Put a covid-19 image and create cards that explains the number of infected, death, and recovered corona cases

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
+import { fetchDataByCountries } from "./api";
+
+import useStyles from "./styles";
+import covidImage from "../src/images/covid.png";
+//
+//
 function App() {
+  const fetchedDataByCountries = fetchDataByCountries();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className></div>
+    </>
   );
 }
 
